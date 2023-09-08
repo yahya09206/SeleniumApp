8ce55296077d2ffe4aa20c76fe8c901422fb40cd
Yahya <yahyahussein91@gmail.com>
2023-09-07 23:39:53 -0700

    added chrome driver

--- src/main/java/org/yahya/Main.java
+++ src/main/java/org/yahya/Main.java
@@ -1,7 +1,10 @@
 package org.yahya;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
 
+        ChromeDriver driver = new ChromeDriver();
     }
 }
